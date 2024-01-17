b4a46bd1499db7222184a1971fc5e5486977fae5
sahil3982 <113926133+Sahil3982@users.noreply.github.com>
2024-01-17 15:47:48 +0530

    15:47

--- ToDay/first/src/App.jsx
+++ ToDay/first/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "../Components/Header/Header";
+import './index.css'
+import './App.css'
 const App = ()=>{
   return(
     <>
       <h1 className="myclass">Hey</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
